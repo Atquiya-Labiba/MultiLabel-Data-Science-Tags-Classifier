920cbcd6a6e0333450be6665682f8bb4cdd3947d
Atquiya-Labiba <108298799+Atquiya-Labiba@users.noreply.github.com>
2025-11-06 21:27:55 +0600

    Updated examples

--- deployment/app.py
+++ deployment/app.py
@@ -29,7 +29,7 @@ demo = gr.Interface(
     outputs=gr.Label(num_top_classes=5), 
     examples = [    
     "I want to develop a machine learning model that predicts the correct medicine dosage required to keep a specific lab value within the target range of 5 to 7. I also have several other predictor variables available. I am unsure which machine learning algorithm would be most suitable for deployment and use with future patients. Additionally, should I define the outcome as binary (1 if the value is between 5 and 7, and 0 otherwise), or is there a better approach?",
-    "What is the best way to evaluate performance of Generative Adverserial Network (GAN)? Perhaps measuring the distance between two distributions or maybe something else?"
+    "What is the best way to evaluate performance of Generative Adverserial Network (GAN)? Perhaps measuring the distance between two distributions or maybe something else?",
     "Suppose that I have a file which has thousands of skills starting from A-Z. Now, I would like to create a model that can group similar skills together (example neural network and SVM can group together). I know that I can use NLP for this problem, but I'm not sure about the algorithm that I can use to get the best result."
 ],       
 )
